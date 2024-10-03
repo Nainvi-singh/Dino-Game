c98bf81c3ab94a5cd3972a5bed8c38f0b6ba733b
Nainvi-singh <1000015922@dit.edu.in>
2024-10-03 20:34:04 +0530

    update commit

--- script/player.js
+++ script/player.js
@@ -1,4 +1,4 @@
-import { GAME_HEIGHT } from "./config.js";
+import { GAME_HEIGHT, GRAVITY } from "./config.js";
 
 export default class Player{
     constructor(){
@@ -7,9 +7,38 @@ export default class Player{
         this.w=88;
         this.y=GAME_HEIGHT-(this.h+20);
         this.image=new Image();
-        this.image.src='../assets/still.png';
+        // this.image.src='../assets/still.png';
+        this.runImages=['../assets/run1.png','../assets/run2.png'];
+        this.index=0;
+        this.image.src=this.runImages[this.index];
+        this.isJumping=false;
+    }
+    jump(){
+        if(!this.isJumping){
+        this.y=this.y-(GAME_HEIGHT-this.h);
+        this.isJumping=true;
+        }   
+    }
+    fall(){
+        const FLOOR=GAME_HEIGHT-this.h-20 ;
+        if(this.y>=FLOOR){
+            this.isJumping=false;
+            return;  
+        }else{
+            this.y=this.y+GRAVITY;
+        }
+        
     }
     draw(context){
         context.drawImage(this.image,this.x,this.y,this.w,this.h);
+        this.walkAnimation();
+        this.fall();
+    }
+    walkAnimation(){
+        if(this.index>=2){
+            this.index=0;
+        }
+        this.image.src=this.runImages[this.index];
+        this.index++;
     }
 }
