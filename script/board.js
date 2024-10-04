ea309373f18b1142f86b8838a144820d80fde1b4
Nainvi-singh <1000015922@dit.edu.in>
2024-10-04 19:36:19 +0530

    game over

--- script/board.js
+++ script/board.js
@@ -10,28 +10,28 @@ import Player from "./player.js";
 // window (tab)
 window.addEventListener('load', gameStart);
 let player;
-let context ;
+let context;
 let floor;
-function gameStart(){
+function gameStart() {
     bindEvents();
     prepareCanvas();
     loadSprites();
     gameLoop();
 }
 
-function bindEvents(){
-    window.addEventListener('keyup',doJump);
+function bindEvents() {
+    window.addEventListener('keyup', doJump);
 }
-function doJump(event){
-    console.log('Event Code ',event.code);
-    if(event.code === 'Space'){
+function doJump(event) {
+    console.log('Event Code ', event.code);
+    if (event.code === 'Space') {
         player.jump();
 
     }
 
 }
 
-function prepareCanvas(){
+function prepareCanvas() {
     const canvas = document.querySelector('#canvas');
     canvas.width = GAME_WIDTH;
     canvas.height = GAME_HEIGHT;
@@ -39,7 +39,7 @@ function prepareCanvas(){
     //canvas.style = 'border: 1px solid black';
 }
 
-function loadSprites(){
+function loadSprites() {
     player = new Player();
     floor = new Floor();
     loadCactus();
@@ -48,58 +48,103 @@ function loadSprites(){
 
 
 let cactusArray = [];
-function loadCactus(){
-    const cactusArr = ['../assets/cactus1.png','../assets/cactus2.png','../assets/cactus3.png'];
-    let GAP  = 1 ;
-    
-    for(var c of cactusArr){
-        const cactus = new Cactus(GAME_WIDTH * GAP, GAME_HEIGHT , 48, 100, c);
+function loadCactus() {
+    const cactusArr = ['../assets/cactus1.png', '../assets/cactus2.png', '../assets/cactus3.png'];
+    let GAP = 1;
+
+    for (var c of cactusArr) {
+        const cactus = new Cactus(GAME_WIDTH * GAP, GAME_HEIGHT, 48, 100, c);
         GAP++;
         cactusArray.push(cactus);
     }
 }
 
-function generateRandomNumber(){
-    return Math.floor(Math.random() * MAX - MIN + 1) + MIN; 
+function generateRandomNumber() {
+    return Math.floor(Math.random() * MAX - MIN + 1) + MIN;
 }
 
 let delay = 0;
-function generateRandomCactus(){
-    if(delay >=70){
+function generateRandomCactus() {
+    if (delay >= 70) {
         delay = 0;
-    setTimeout(()=>{
-        loadCactus();
-       // cactusArray.push(new Cactus(GAME_WIDTH * 1, GAME_HEIGHT , 48, 100, '../assets/cactus1.png')
-    }, generateRandomNumber());
-}
-delay++;
+        setTimeout(() => {
+            loadCactus();
+            // cactusArray.push(new Cactus(GAME_WIDTH * 1, GAME_HEIGHT , 48, 100, '../assets/cactus1.png')
+        }, generateRandomNumber());
+    }
+    delay++;
 }
 
-function printCactus(context){
+function printCactus(context) {
     // console.log('Cactus Array Size ', cactusArray.length);
-    for(let cactus of cactusArray){
+    for (let cactus of cactusArray) {
         cactus.draw(context);
     }
 }
 
-function removeUnUsedCactus(){
-    cactusArray = cactusArray.filter(c=>!c.isOutOfScreen());
+function removeUnUsedCactus() {
+    cactusArray = cactusArray.filter(c => !c.isOutOfScreen());
 }
-
-function gameLoop(){
+function printGameOver() {
+    context.font = 'bold 48px serif'
+    context.fillStyle = 'grey';
+    context.fillText('Game Over', GAME_WIDTH / 3, GAME_HEIGHT / 2);
+}
+function gameLoop() {
     //console.log('Game Loop');
     clearScreen();
-    player.draw(context);
-    floor.draw(context);
-    printCactus(context);
-    generateRandomCactus();
-    removeUnUsedCactus();
-    setTimeout(function(){
-        requestAnimationFrame(gameLoop);
-    }, FRAME_RATE);
+    if (isCollisionHappen()) {
+        player.draw(context);
+        floor.draw(context);
+        printCactus(context);
+        generateRandomCactus();
+        removeUnUsedCactus();
+        printGameOver();
+        score();
+    }
+    else {
+        player.draw(context);
+        floor.draw(context);
+        printCactus(context);
+        generateRandomCactus();
+        removeUnUsedCactus();
+        score();
+
+        setTimeout(function () {
+            requestAnimationFrame(gameLoop);
+        }, FRAME_RATE);
+    }
+}
+let scoreValue=0;
+function score(){
+    if(!localStorage.maxScore){
+        localStorage.maxScore=scoreValue;
+    }
+    if(scoreValue>localStorage.maxScore){
+        localStorage.maxScore=scoreValue;
+    }
+    scoreValue++;
+    context.font = 'bold 20px serif'
+    context.fillStyle = 'grey';
+    context.fillText(scoreValue.toString().padStart(5,0), GAME_WIDTH -100, 40);
+    context.fillText(localStorage.maxScore.toString().padStart(5,0), GAME_WIDTH -200, 40);
 }
 
-function clearScreen(){
+function clearScreen() {
     context.fillStyle = 'white';
-    context.fillRect(0,0, GAME_WIDTH, GAME_HEIGHT);
+    context.fillRect(0, 0, GAME_WIDTH, GAME_HEIGHT);
+}
+
+function isCollide(cactus) {
+    return player.x < cactus.x + cactus.w && player.x + player.w > cactus.x
+        && player.y < cactus.y + cactus.h && player.y + player.h > cactus.y;
+}
+function isCollisionHappen() {
+    return cactusArray.some(c => isCollide(c));
+    // for(let cactus of cactusArray){
+    //     if(isCollide(cactus)){
+    //         return true;
+    //     }
+    // }
+    // return false;
 }
